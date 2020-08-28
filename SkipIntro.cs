9741172671be3ecb8510ceafd459164deed76148
gnivler <gnivil@gmail.com>
2020-08-27 22:35:55 -0600

    1.3.1

--- SkipIntro.cs
+++ SkipIntro.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using EasyHook;
 using UnityModManagerNet;
-using Harmony12;
+using HarmonyLib;
 using Kingmaker;
+using Kingmaker.UI._ConsoleUI.Endless.GameOver;
 using Kingmaker.UI.MainMenuUI;
 using static SkipIntro.Main;
 
-// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedMember.Local 
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedType.Local 
 // ReSharper disable InconsistentNaming
 
 namespace SkipIntro
 {
     internal static class Main
     {
-        internal static readonly HarmonyInstance harmony = HarmonyInstance.Create("ca.gnivler.SkipIntro");
+        internal static readonly Harmony harmony = new Harmony("ca.gnivler.SkipIntro");
+
 
         private static bool OnToggle(UnityModManager.ModEntry mod_entry, bool value)
         {
@@ -33,6 +41,20 @@ namespace SkipIntro
             mod_entry.OnToggle = OnToggle;
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             ManualPatches.Init();
+            try
+            {
+                //var hook = LocalHook.Create(
+                //    LocalHook.GetProcAddress("UnityPlayer.dll", "BeginSplashScreen"),
+                //    new EasyHook.BeginSplashDelegate(EasyHook.BeginSplashScreenHook),
+                //    null);
+                //
+                //Log("HOOK" + hook);
+                //hook.ThreadACL.SetInclusiveACL(new[] {0});
+            }
+            catch (Exception ex)
+            {
+                Log(ex);
+            }
         }
 
         internal static void Log(object input)
@@ -82,9 +104,10 @@ namespace SkipIntro
             var startGameCoroutine = typeof(GameStarter).GetNestedTypes(AccessTools.all).First(x => x.Name.Contains("StartGameCoroutine"));
             var original = AccessTools.Method(startGameCoroutine, "MoveNext");
             var transpiler = AccessTools.Method(typeof(ManualPatches), nameof(StartGameCoroutine_Transpiler));
+            var postfix = AccessTools.Method(typeof(ManualPatches), nameof(StartGameCoroutine_Postfix));
             try
             {
-                harmony.Patch(original, null, null, new HarmonyMethod(transpiler));
+                harmony.Patch(original, null, new HarmonyMethod(postfix), new HarmonyMethod(transpiler));
             }
             catch (Exception ex)
             {
@@ -110,8 +133,16 @@ namespace SkipIntro
             try
             {
                 codes = instructions.ToList();
-                var index = codes.FindIndex(x => x.opcode == OpCodes.Newobj);
-                codes[index - 1].operand = 0f;
+                var index = codes.FindIndex(x =>
+                    x.opcode == OpCodes.Ldfld &&
+                    x.operand.ToString().Contains(@"<>8__1"));
+                index++;
+                codes[index].opcode = OpCodes.Ldc_I4_1;
+                Log("========================== First");
+                codes.GetRange(index - 5, 10).Do(x => Log($"{x.opcode,-20}{x.operand}")); //--> {(x.labels.Count > 0 ? x.labels[0].ToString() : ""),20}"));
+                codes[index].operand = 0f;
+                Log("========================== Last");
+                codes.GetRange(index - 5, 10).Do(x => Log($"{x.opcode,-20}{x.operand}")); // --> {(x.labels.Count > 0 ? x.labels[0].ToString() : ""),20}"));
             }
             catch (Exception ex)
             {
@@ -119,6 +150,28 @@ namespace SkipIntro
             }
 
             return codes.AsEnumerable();
+        }
+
+        private static void StartGameCoroutine_Postfix()
+        {
+            try
+            {
+                var controls = new FileInfo(Path.Combine(Assembly.GetExecutingAssembly().Location + "controls.txt"));
+                if (File.Exists(controls.FullName))
+                {
+                    var input = File.ReadLines(controls.FullName).First().ToLowerInvariant();
+                    Game.Instance.ControllerMode = input == "mouse" ? Game.ControllerModeType.Mouse : Game.ControllerModeType.Gamepad;
+                }
+                else
+                {
+                    Game.Instance.ControllerMode = Game.ControllerModeType.Mouse;
+                }
+            }
+            catch (Exception ex)
+            {
+                Game.Instance.ControllerMode = Game.ControllerModeType.Mouse;
+                Log(ex);
+            }
         }
 
         // removes the fade out of the logo after loading is complete
@@ -134,7 +187,6 @@ namespace SkipIntro
             codes[index - 1].opcode = OpCodes.Nop;
             codes[index].opcode = OpCodes.Ldc_R4;
             codes[index].operand = 0f;
-
             return codes.AsEnumerable();
         }
     }
